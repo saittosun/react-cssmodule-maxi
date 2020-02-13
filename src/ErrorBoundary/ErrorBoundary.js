24499eef3d1c43d624d3c824d1ed48e3fe7ad86e
Tosun Sait <tosunsa@cronos.be>
2020-02-13 16:53:30 +0100

    error boundary which is a new property of react16+

---
+++ src/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,24 @@
+// jshint esversion: 6
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  state = {
+    hasError: false,
+    errorMessage: ''
+  }
+
+  componentDidCatch = (error, info) => {
+    this.setState({hasError: true, errorMessage: error});
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h1>{this.state.errorMessage}</h1>
+      )
+    } else {
+      return this.props.children;
+    }
+  }
+}
+
+export default ErrorBoundary;
